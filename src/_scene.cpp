9b188b06eec6c09e86a39cd4e057fd77a016aeda
mousebytes <alfonso.morales625625@gmail.com>
2025-05-01 18:13:04 -0700

    m

--- src/_scene.cpp
+++ src/_scene.cpp
@@ -689,6 +689,7 @@ void _scene::check_enemy_collisions()
         }
         else if (isTouching && player->player_can_be_damaged)
         {
+            sndOof->playSounds("oof.mp3");
             player->health--;
             player->damage_timer->reset();
             player->player_can_be_damaged = false;

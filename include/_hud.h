1722486301b4be0170bb1b1d5f503816297ff065
mousebytes <alfonso.morales625625@gmail.com>
2025-05-02 05:08:26 -0700

    banana counter

--- include/_hud.h
+++ include/_hud.h
@@ -3,6 +3,9 @@
 
 #include<_common.h>
 #include<_textureLoader.h>
+#include<_fonts.h>
+#include<_camera.h>
+#include<_buttons.h>
 
 class _hud
 {
@@ -15,14 +18,21 @@ class _hud
     void initHud(char* heartFile,int,int,vec3);
     void drawHearts(int health, vec3 camPos);
 
+    vec3 vert[4];
+    float xMin,xMax,yMin,yMax;
     vec3 pos;
     vec3 scl;
 
     int framesX;
     int framesY;
 
-    float xMax,yMax,xMin,yMin;
-    vec3 vert[4];
+    _fonts *bananaCount = new _fonts();
+
+    void updateBananaCount(int bananas, _camera*);
+
+    _buttons *bananaIcon = new _buttons();
+
+    void drawBananaIcon(_camera*);
 
     protected:
 
